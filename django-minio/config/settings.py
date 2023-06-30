48cd9d8f3c7b37fd805052ec0632f07a404fbe3e
you@example.com <you@example.com>
2023-06-30 12:05:52 -0500

    config postgres

--- django-minio/config/settings.py
+++ django-minio/config/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'persona',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': '192.168.1.201',
+        'PORT': '5432',
     }
 }
 CORS_ALLOW_ALL_ORIGINS = True
